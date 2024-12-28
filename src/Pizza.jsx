86c1cb3b837a3093157de05988dc8a629c1d415c
Victor Volovik <VictorVolovik@users.noreply.github.com>
2024-12-28 11:12:27 +0400

    Add example tests for Pizza component

--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -1,9 +1,11 @@
+const FALLBACK_IMG_URL = "https://picsum.photos/200";
+
 const Pizza = ({ name, description, image }) => {
   return (
     <div className="pizza">
       <h2>{name}</h2>
       <p>{description}</p>
-      <img src={image} alt={name} />
+      <img src={image ?? FALLBACK_IMG_URL} alt={name} />
     </div>
   );
 };
